a88d50182baaea70f2bed9a6a45a62bdf9ce4368
Maksym7415 <popovmaksim7415@gmail.com>
2021-03-02 23:43:09 +0200

    changed succes url

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ app.post('/api/subscription', async (req, res) => {
       // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
       // the actual Session ID is returned in the query parameter when your customer
       // is redirected to the success page.
-      success_url: 'https://eat-beat.hopto.org/settings/billing/success',
-      cancel_url: 'https://eat-beat.hopto.org//settings/billing/error',
+      success_url: 'https://eat-beat.hopto.org',
+      cancel_url: 'https://eat-beat.hopto.org',
     });
 
     res.send({
